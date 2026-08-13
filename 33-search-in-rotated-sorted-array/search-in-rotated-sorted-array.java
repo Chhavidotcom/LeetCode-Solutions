e5b3d8ec600625fbbfde9c250789d9d78c2c2a53
Chhavi Gupta <68819893+Chhavidotcom@users.noreply.github.com>
2026-08-13 23:04:00 +0530

    Time: 0 ms (100.00%) | Memory: 43.7 MB (82.66%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -1,18 +1,21 @@
 class Solution {
     public int search(int[] nums, int target) {
-int pivot= findPivot(nums);
-if (target >= nums[pivot] && target <= nums[nums.length - 1]) {
-return binarySearch(nums, pivot, nums.length - 1, target);
-        }
-else
-return binarySearch(nums, 0, pivot - 1, target);
-        }
 
+        int pivot = findPivot(nums);
 
-public int findPivot(int[] nums){
-    int start=0;
-int end=nums.length-1;
-   if (nums[start] <= nums[end]) {
+        if (target >= nums[pivot] && target <= nums[nums.length - 1]) {
+            return binarySearch(nums, pivot, nums.length - 1, target);
+        } else {
+            return binarySearch(nums, 0, pivot - 1, target);
+        }
+    }
+
+    public int findPivot(int[] nums) {
+
+        int start = 0;
+        int end = nums.length - 1;
+
+        if (nums[start] <= nums[end]) {
             return 0;
         }
 
@@ -20,23 +23,27 @@ int end=nums.length-1;
 
             int mid = start + (end - start) / 2;
 
-            // Pivot mil gaya
-            if (mid > 0 && nums[mid] < nums[mid - 1]) {
-                return mid;
+         
+            if (mid < nums.length - 1 &&
+                nums[mid] > nums[mid + 1]) {
+                return mid + 1;
             }
-
             if (nums[mid] >= nums[0]) {
                 start = mid + 1;
-            } else {
+            } 
+            else {
                 end = mid - 1;
             }
         }
 
         return 0;
     }
-    public int binarySearch(int[] nums,int start,int end,int target){
-    while(start<=end){
-    int mid = start + (end - start) / 2;
+
+    public int binarySearch(int[] nums, int start, int end, int target) {
+
+        while (start <= end) {
+
+            int mid = start + (end - start) / 2;
 
             if (nums[mid] == target) {
                 return mid;
@@ -44,10 +51,12 @@ int end=nums.length-1;
 
             if (nums[mid] < target) {
                 start = mid + 1;
-            } else {
+            } 
+            else {
                 end = mid - 1;
             }
         }
 
         return -1;
-}}
+    }
+}
