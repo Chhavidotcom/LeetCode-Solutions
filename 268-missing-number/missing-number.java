2f64ec8f02f30c36bb25b65f4d8c08af5c0aaf58
Chhavi Gupta <68819893+Chhavidotcom@users.noreply.github.com>
2026-08-18 21:32:18 +0530

    Time: 0 ms (100.00%) | Memory: 47.3 MB (67.02%) - LeetSync

--- 268-missing-number/missing-number.java
+++ 268-missing-number/missing-number.java
@@ -1,15 +1,16 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        HashMap<Integer,Integer>map = new HashMap<>();
-      for(int i =0;i<nums.length;i++){
-        map.put(nums[i],i);
-      }  
-      for(int i =0;i<=nums.length;i++){
-        if(map.containsKey(i)){
-            continue;
+
+        int n = nums.length;
+
+        int expectedSum = n * (n + 1) / 2;
+
+        int actualSum = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            actualSum += nums[i];
         }
-        return i;
-      }
-      return 0;
+
+        return expectedSum - actualSum;
     }
 }
