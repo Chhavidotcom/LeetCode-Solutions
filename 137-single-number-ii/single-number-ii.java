0f920c086c22fd35c691609b296bde2597823f29
Chhavi Gupta <68819893+Chhavidotcom@users.noreply.github.com>
2026-09-05 18:30:58 +0530

    Time: 4 ms (65.74%) | Memory: 45.3 MB (51.31%) - LeetSync

--- 137-single-number-ii/single-number-ii.java
+++ 137-single-number-ii/single-number-ii.java
@@ -1,21 +1,29 @@
 class Solution {
     public int singleNumber(int[] nums) {
-
-        int ans = 0;
-
-        for(int i = 0; i < 32; i++) {
-
-            int count = 0;
-
-            for(int num : nums) {
-                count += (num >> i) & 1;
-            }
-
-            count = count % 3;
-
-            ans = ans | (count << i);
-        }
-
-        return ans;
+        int ans =0;
+        for(int i =0;i<32;i++){
+            int count0=0;
+            int count1=0;
+            for(int j =0;j<nums.length;j++)
+{
+    if((nums[j]&(1<<i))==0){
+        count0++;
     }
+    else{
+        count1++;
+    }
+}  
+
+if(count1 % 3 == 1) {
+
+                if(i == 31) {
+                    ans = ans + Integer.MIN_VALUE;
+                }
+                else {
+                    ans = ans + (int)Math.pow(2, i);
+                }
+    
+    }
+        }
+      return ans;}
 }
