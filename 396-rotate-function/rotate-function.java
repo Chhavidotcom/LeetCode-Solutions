2af8c018ecedc85de0a48440aa02047ada181848
Chhavi Gupta <68819893+Chhavidotcom@users.noreply.github.com>
2026-09-19 23:58:21 +0530

    Time: 4 ms (98.72%) | Memory: 91.3 MB (32.31%) - LeetSync

---
+++ 396-rotate-function/rotate-function.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int maxRotateFunction(int[] nums) {
+
+        int n = nums.length;
+
+        long sum = 0;
+        long f = 0;
+
+        // F(0) calculate karo
+        for (int i = 0; i < n; i++) {
+            sum += nums[i];
+            f += (long) i * nums[i];
+        }
+
+        long max = f;
+
+        // F(k) from F(k-1)
+        for (int k = 1; k < n; k++) {
+
+            f = f + sum - (long) n * nums[n - k];
+
+            max = Math.max(max, f);
+        }
+
+        return (int) max;
+    }
+}
